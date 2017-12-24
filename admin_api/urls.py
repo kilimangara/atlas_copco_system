c3c523172ed3fd6881d80cd537014c527c5790f4
nikitazlain <kilimangara@yandex.ru>
2017-12-24 15:08:42 +0300

    fix

--- admin_api/urls.py
+++ admin_api/urls.py
@@ -2,10 +2,10 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-    url(r'^users', views.user_list),
-    url(r'^products', views.products_list),
-    url(r'^accounts', views.account_list),
-    url(r'^products/(?P<product_id>\d+)', views.crud_product),
-    url(r'^users/(?P<user_id>\d+)', views.crud_user),
-    url(r'^accounts/(?P<account_id>\d+)', views.crud_account),
+    url(r'^users$', views.user_list),
+    url(r'^products$', views.products_list),
+    url(r'^accounts$', views.account_list),
+    url(r'^products/(?P<product_id>\d+)$', views.crud_product),
+    url(r'^users/(?P<user_id>\d+)$', views.crud_user),
+    url(r'^accounts/(?P<account_id>\d+)$', views.crud_account),
 ]
